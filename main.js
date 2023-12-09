998715d0a4095b761aa3e1bc72a59676fd0c4e16
Nikethapawar-19 <nikethapawar1995@gmail.com>
2023-12-09 14:33:55 +0530

    mainfile

---
+++ main.js
@@ -0,0 +1,34 @@
+let express = require("express")
+let app=express();
+const { default: mongoose } = require("mongoose");
+const myinfo = require("./server/models/MyInfoModel");
+const dotenv = require("dotenv")
+dotenv.config();
+let bodyParser = require('body-parser');
+
+let DB_URL =mongoose.connect(process.env.DB_URL)
+
+app.get('/',function(req,res){
+    res.send("hello niketha")
+})
+
+app.post('/',function(req,res){
+    try {
+        let body = req.body;
+        console.log(body)
+        let create_data = new myinfo(body)
+        
+        create_data.save();
+        return res.status(200).json({myinfo:create_data})
+          
+    } catch (error) {
+        return res.status(500).json({error:"unknown error",error_code:"unknown_error"})
+    }
+    
+
+})
+
+app.listen('7000',function(){
+    console.log('listening to port 7000')
+})
+module.exports = app
